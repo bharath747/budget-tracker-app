5d9cdc49dbb95141575786cb914c9f7bcb350163
Bharath <bharathsimha747@gmail.com>
2026-09-23 23:03:22 +0530

    Fix Room migration imports

--- app/src/main/java/com/bharath/budgettracker/data/BudgetDatabase.kt
+++ app/src/main/java/com/bharath/budgettracker/data/BudgetDatabase.kt
@@ -1,5 +1,7 @@
 package com.bharath.budgettracker.data
 import androidx.room.*
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.flow.Flow
 
